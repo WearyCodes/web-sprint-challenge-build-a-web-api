f71359a6c8834f9838c22d288bd44f2f66cb5b41
Samuel Sanchez <wearycodes@gmail.com>
2024-03-19 15:27:08 -0500

    13 of 27 working tests for sprint challenge

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,51 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model')
+
+async function validateProjectId(req, res, next){
+    const { id } = req.params
+    await Projects.get(id)
+    .then(project => {
+        if (!project) {
+            res.status(404)
+        } else {
+            req.project = project
+            next()
+        }
+    })
+    .catch(err => {
+        res.status(404)
+    })
+    next()
+}
+
+// - [ ] `[POST] /api/projects`
+//   - Returns the newly created project as the body of the response.
+//   - If the request body is missing any of the required fields it responds with a status code 400.
+// | Field       | Data Type | Metadata                                                                    |
+// | ----------- | --------- | --------------------------------------------------------------------------- |
+// | id          | number    | do not provide it when creating projects, the database will generate it     |
+// | name        | string    | required                                                                    |
+// | description | string    | required                                                                    |
+// | completed   | boolean   | not required, defaults to false when creating projects                      |
+async function validatePost (req, res, next) {
+    console.log(req.method)
+    const { name, description, completed} = req.body
+    if (name && description){
+        req.body = {
+            name: name,
+            description: description,
+            completed: completed ? completed : false
+        }
+        next()
+    } else {
+        res.status(400).json({
+            message: 'Missing name, description, or completed'
+        })
+    }
+}
+
+
+module.exports = {
+    validateProjectId,
+    validatePost,
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,39 @@
-// Write your "projects" router here!
+// Write your "projects" router here
+const express = require('express')
+const Projects = require('./projects-model')
+const router = express.Router()
+const { validateProjectId, validatePost } = require('./projects-middleware')
+
+router.get('/', async (req, res, next) => {
+    Projects.get()
+        .then(promise => {
+            if (!promise) {
+                res.send([])
+            } else {
+                res.send(promise)
+            }
+        })
+        .catch(err => console.log(err))
+})
+
+
+router.get('/:id', validateProjectId, async (req, res, next) => {
+    res.send(req.project)
+    next()
+})
+
+router.post('/', validatePost, (req, res, next) => {
+     Projects.insert(req.body)
+    .then(project => {
+        res.status(201).json(project)
+    })
+    .catch(err => console.log(err))
+})
+
+router.put('/:id', validatePost, (req, res, next) => {
+    Projects.update(req.params.id, req.body)
+    .then(project => res.status(201).json(project))
+    .catch(err=> console.log(err))
+})
+
+module.exports = router
